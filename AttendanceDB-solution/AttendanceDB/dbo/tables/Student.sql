603666b54e15c0f707e7aa8e166da3ed91b5cc30
Profiliu <muaprof2@gmail.com>
2021-10-04 21:33:18 +0200

    Added student table

---
+++ AttendanceDB-solution/AttendanceDB/dbo/tables/Student.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[studentId] INT NOT NULL PRIMARY KEY, 
+    [firstName] VARCHAR(25) NOT NULL, 
+    [middleName] VARCHAR(25) NULL, 
+    [lastName] VARCHAR(25) NOT NULL, 
+    [studentEmail] VARCHAR(50) NOT NULL, 
+    [studentPassword] VARCHAR(50) NOT NULL, 
+    [studentMobile] CHAR(10) NOT NULL, 
+    [role] VARCHAR(10) NOT NULL
+)
