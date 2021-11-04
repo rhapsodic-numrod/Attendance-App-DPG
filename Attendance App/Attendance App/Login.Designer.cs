ede6c0720b2a7733b445837074b5fcda06e9e78b
Profiliu <muaprof2@gmail.com>
2021-11-04 19:00:46 +0200

    Created login form ui (Still need to add login logic)

---
+++ Attendance App/Attendance App/Login.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Attendance_App
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLogo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.adminRadio = new System.Windows.Forms.RadioButton();
+            this.lecturerRadio = new System.Windows.Forms.RadioButton();
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.studentRadio = new System.Windows.Forms.RadioButton();
+            this.checkBoxViewPassword = new System.Windows.Forms.CheckBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginLogo
+            // 
+            this.loginLogo.AutoSize = true;
+            this.loginLogo.Font = new System.Drawing.Font("Roboto", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLogo.ForeColor = System.Drawing.Color.White;
+            this.loginLogo.Location = new System.Drawing.Point(171, 109);
+            this.loginLogo.Name = "loginLogo";
+            this.loginLogo.Size = new System.Drawing.Size(108, 46);
+            this.loginLogo.TabIndex = 0;
+            this.loginLogo.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(78, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(78, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // adminRadio
+            // 
+            this.adminRadio.AutoSize = true;
+            this.adminRadio.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminRadio.ForeColor = System.Drawing.Color.White;
+            this.adminRadio.Location = new System.Drawing.Point(305, 192);
+            this.adminRadio.Name = "adminRadio";
+            this.adminRadio.Size = new System.Drawing.Size(70, 24);
+            this.adminRadio.TabIndex = 4;
+            this.adminRadio.TabStop = true;
+            this.adminRadio.Text = "Admin";
+            this.adminRadio.UseVisualStyleBackColor = true;
+            this.adminRadio.CheckedChanged += new System.EventHandler(this.adminRadio_CheckedChanged);
+            // 
+            // lecturerRadio
+            // 
+            this.lecturerRadio.AutoSize = true;
+            this.lecturerRadio.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lecturerRadio.ForeColor = System.Drawing.Color.White;
+            this.lecturerRadio.Location = new System.Drawing.Point(179, 192);
+            this.lecturerRadio.Name = "lecturerRadio";
+            this.lecturerRadio.Size = new System.Drawing.Size(82, 24);
+            this.lecturerRadio.TabIndex = 5;
+            this.lecturerRadio.TabStop = true;
+            this.lecturerRadio.Text = "Lecturer";
+            this.lecturerRadio.UseVisualStyleBackColor = true;
+            this.lecturerRadio.CheckedChanged += new System.EventHandler(this.lecturerRadio_CheckedChanged);
+            // 
+            // txtBoxID
+            // 
+            this.txtBoxID.Location = new System.Drawing.Point(82, 262);
+            this.txtBoxID.Name = "txtBoxID";
+            this.txtBoxID.Size = new System.Drawing.Size(279, 25);
+            this.txtBoxID.TabIndex = 1;
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(82, 338);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.PasswordChar = '*';
+            this.txtBoxPassword.Size = new System.Drawing.Size(279, 25);
+            this.txtBoxPassword.TabIndex = 2;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(180)))), ((int)(((byte)(189)))));
+            this.loginBtn.FlatAppearance.BorderSize = 0;
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.Location = new System.Drawing.Point(167, 425);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(113, 41);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // studentRadio
+            // 
+            this.studentRadio.AutoSize = true;
+            this.studentRadio.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentRadio.ForeColor = System.Drawing.Color.White;
+            this.studentRadio.Location = new System.Drawing.Point(68, 192);
+            this.studentRadio.Name = "studentRadio";
+            this.studentRadio.Size = new System.Drawing.Size(78, 24);
+            this.studentRadio.TabIndex = 6;
+            this.studentRadio.TabStop = true;
+            this.studentRadio.Text = "Student";
+            this.studentRadio.UseVisualStyleBackColor = true;
+            this.studentRadio.CheckedChanged += new System.EventHandler(this.studentRadio_CheckedChanged);
+            // 
+            // checkBoxViewPassword
+            // 
+            this.checkBoxViewPassword.AutoSize = true;
+            this.checkBoxViewPassword.ForeColor = System.Drawing.Color.White;
+            this.checkBoxViewPassword.Location = new System.Drawing.Point(249, 386);
+            this.checkBoxViewPassword.Name = "checkBoxViewPassword";
+            this.checkBoxViewPassword.Size = new System.Drawing.Size(124, 23);
+            this.checkBoxViewPassword.TabIndex = 7;
+            this.checkBoxViewPassword.Text = "View Password";
+            this.checkBoxViewPassword.UseVisualStyleBackColor = true;
+            this.checkBoxViewPassword.CheckedChanged += new System.EventHandler(this.checkBoxViewPassword_CheckedChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Crimson;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(364, 28);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(32, 32);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "✖";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.loginBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(75)))), ((int)(((byte)(84)))));
+            this.ClientSize = new System.Drawing.Size(431, 561);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.checkBoxViewPassword);
+            this.Controls.Add(this.studentRadio);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.txtBoxID);
+            this.Controls.Add(this.lecturerRadio);
+            this.Controls.Add(this.adminRadio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginLogo);
+            this.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton adminRadio;
+        private System.Windows.Forms.RadioButton lecturerRadio;
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.RadioButton studentRadio;
+        private System.Windows.Forms.CheckBox checkBoxViewPassword;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Attendance App/Attendance App/Login.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_App
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public int Role { get; set; }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if (Role == 1) //Student
+            {
+                MessageBox.Show("Welcome back " + txtBoxID.Text);
+            } 
+            else if (Role == 2) //Lecturer
+            {
+                MessageBox.Show("Welcome back " + txtBoxID.Text);
+            }
+            else if (Role == 3) //Admin
+            {
+                MessageBox.Show("Welcome back " + txtBoxID.Text);
+            }
+        }
+        #region Role radio selection
+        private void studentRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            Role = 1;
+        }
+
+        private void lecturerRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            Role = 2;
+        }
+
+        private void adminRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            Role = 3;
+        }
+        #endregion
+
+        private void checkBoxViewPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtBoxPassword.PasswordChar == '*')
+            {
+                txtBoxPassword.PasswordChar = '\0'; //if you will see the password as text instead of a *
+            }
+            else
+            {
+                txtBoxPassword.PasswordChar = '*';
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
