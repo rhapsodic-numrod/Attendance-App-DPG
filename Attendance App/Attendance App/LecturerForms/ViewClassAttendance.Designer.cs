5cf925e564e3251d20aceb19e333526e0f657771
Profiliu <muaprof2@gmail.com>
2021-11-06 01:01:01 +0200

    Added Lecturer View attendance (no logic)

---
+++ Attendance App/Attendance App/LecturerForms/ViewClassAttendance.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Attendance_App.LecturerForms
+{
+    partial class ViewClassAttendance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCloseForm = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxCourse = new System.Windows.Forms.ComboBox();
+            this.ViewAttendanceText = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCloseForm
+            // 
+            this.btnCloseForm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCloseForm.BackColor = System.Drawing.Color.Crimson;
+            this.btnCloseForm.FlatAppearance.BorderSize = 0;
+            this.btnCloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseForm.ForeColor = System.Drawing.Color.White;
+            this.btnCloseForm.Location = new System.Drawing.Point(681, 21);
+            this.btnCloseForm.Name = "btnCloseForm";
+            this.btnCloseForm.Size = new System.Drawing.Size(32, 32);
+            this.btnCloseForm.TabIndex = 15;
+            this.btnCloseForm.Text = "✖";
+            this.btnCloseForm.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(168)))), ((int)(((byte)(172)))));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(97, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(569, 383);
+            this.panel1.TabIndex = 16;
+            // 
+            // comboBoxCourse
+            // 
+            this.comboBoxCourse.FormattingEnabled = true;
+            this.comboBoxCourse.Location = new System.Drawing.Point(97, 57);
+            this.comboBoxCourse.Name = "comboBoxCourse";
+            this.comboBoxCourse.Size = new System.Drawing.Size(134, 24);
+            this.comboBoxCourse.TabIndex = 18;
+            // 
+            // ViewAttendanceText
+            // 
+            this.ViewAttendanceText.AutoSize = true;
+            this.ViewAttendanceText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ViewAttendanceText.Location = new System.Drawing.Point(207, 100);
+            this.ViewAttendanceText.Name = "ViewAttendanceText";
+            this.ViewAttendanceText.Size = new System.Drawing.Size(234, 31);
+            this.ViewAttendanceText.TabIndex = 19;
+            this.ViewAttendanceText.Text = "View Attendance -";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(478, 294);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ViewClassAttendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 611);
+            this.Controls.Add(this.comboBoxCourse);
+            this.Controls.Add(this.ViewAttendanceText);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCloseForm);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ViewClassAttendance";
+            this.Text = "ViewClassAttendance";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCloseForm;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBoxCourse;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label ViewAttendanceText;
+    }
+}
