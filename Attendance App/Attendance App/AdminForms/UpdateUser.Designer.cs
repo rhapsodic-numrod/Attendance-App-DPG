e9f65f5b3d397427f5075cabab358d012b693d99
Profiliu <muaprof2@gmail.com>
2021-11-05 15:18:46 +0200

    Added incomplete Admin Update User Form UI (Still need find out how to change the data)

---
+++ Attendance App/Attendance App/AdminForms/UpdateUser.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace Attendance_App.AdminForms
+{
+    partial class UpdateUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateUserText = new System.Windows.Forms.Label();
+            this.panelUpdateBox = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioAdmin = new System.Windows.Forms.RadioButton();
+            this.radioLecturer = new System.Windows.Forms.RadioButton();
+            this.radioStudent = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCloseForm = new System.Windows.Forms.Button();
+            this.btnUpdateUser = new System.Windows.Forms.Button();
+            this.panelUpdateBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // updateUserText
+            // 
+            this.updateUserText.AutoSize = true;
+            this.updateUserText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateUserText.Location = new System.Drawing.Point(273, 58);
+            this.updateUserText.Name = "updateUserText";
+            this.updateUserText.Size = new System.Drawing.Size(167, 31);
+            this.updateUserText.TabIndex = 0;
+            this.updateUserText.Text = "Update User";
+            // 
+            // panelUpdateBox
+            // 
+            this.panelUpdateBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(168)))), ((int)(((byte)(172)))));
+            this.panelUpdateBox.Controls.Add(this.textBox1);
+            this.panelUpdateBox.Controls.Add(this.radioAdmin);
+            this.panelUpdateBox.Controls.Add(this.radioLecturer);
+            this.panelUpdateBox.Controls.Add(this.radioStudent);
+            this.panelUpdateBox.Controls.Add(this.label1);
+            this.panelUpdateBox.Location = new System.Drawing.Point(93, 119);
+            this.panelUpdateBox.Name = "panelUpdateBox";
+            this.panelUpdateBox.Size = new System.Drawing.Size(560, 406);
+            this.panelUpdateBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter user ID:";
+            // 
+            // radioAdmin
+            // 
+            this.radioAdmin.AutoSize = true;
+            this.radioAdmin.Location = new System.Drawing.Point(403, 18);
+            this.radioAdmin.Name = "radioAdmin";
+            this.radioAdmin.Size = new System.Drawing.Size(65, 21);
+            this.radioAdmin.TabIndex = 15;
+            this.radioAdmin.TabStop = true;
+            this.radioAdmin.Text = "Admin";
+            this.radioAdmin.UseVisualStyleBackColor = true;
+            // 
+            // radioLecturer
+            // 
+            this.radioLecturer.AutoSize = true;
+            this.radioLecturer.Location = new System.Drawing.Point(244, 18);
+            this.radioLecturer.Name = "radioLecturer";
+            this.radioLecturer.Size = new System.Drawing.Size(79, 21);
+            this.radioLecturer.TabIndex = 14;
+            this.radioLecturer.TabStop = true;
+            this.radioLecturer.Text = "Lecturer";
+            this.radioLecturer.UseVisualStyleBackColor = true;
+            // 
+            // radioStudent
+            // 
+            this.radioStudent.AutoSize = true;
+            this.radioStudent.Location = new System.Drawing.Point(89, 18);
+            this.radioStudent.Name = "radioStudent";
+            this.radioStudent.Size = new System.Drawing.Size(75, 21);
+            this.radioStudent.TabIndex = 13;
+            this.radioStudent.TabStop = true;
+            this.radioStudent.Text = "Student";
+            this.radioStudent.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 23);
+            this.textBox1.TabIndex = 16;
+            // 
+            // btnCloseForm
+            // 
+            this.btnCloseForm.BackColor = System.Drawing.Color.Crimson;
+            this.btnCloseForm.FlatAppearance.BorderSize = 0;
+            this.btnCloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseForm.ForeColor = System.Drawing.Color.White;
+            this.btnCloseForm.Location = new System.Drawing.Point(681, 21);
+            this.btnCloseForm.Name = "btnCloseForm";
+            this.btnCloseForm.Size = new System.Drawing.Size(32, 32);
+            this.btnCloseForm.TabIndex = 10;
+            this.btnCloseForm.Text = "✖";
+            this.btnCloseForm.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateUser
+            // 
+            this.btnUpdateUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(180)))), ((int)(((byte)(189)))));
+            this.btnUpdateUser.FlatAppearance.BorderSize = 0;
+            this.btnUpdateUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateUser.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateUser.Location = new System.Drawing.Point(574, 548);
+            this.btnUpdateUser.Name = "btnUpdateUser";
+            this.btnUpdateUser.Size = new System.Drawing.Size(117, 41);
+            this.btnUpdateUser.TabIndex = 11;
+            this.btnUpdateUser.Text = "Update User";
+            this.btnUpdateUser.UseVisualStyleBackColor = false;
+            // 
+            // UpdateUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(744, 611);
+            this.Controls.Add(this.btnUpdateUser);
+            this.Controls.Add(this.btnCloseForm);
+            this.Controls.Add(this.panelUpdateBox);
+            this.Controls.Add(this.updateUserText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "UpdateUser";
+            this.Text = "UpdateUser";
+            this.panelUpdateBox.ResumeLayout(false);
+            this.panelUpdateBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label updateUserText;
+        private System.Windows.Forms.Panel panelUpdateBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioAdmin;
+        private System.Windows.Forms.RadioButton radioLecturer;
+        private System.Windows.Forms.RadioButton radioStudent;
+        private System.Windows.Forms.Button btnCloseForm;
+        private System.Windows.Forms.Button btnUpdateUser;
+    }
+}
