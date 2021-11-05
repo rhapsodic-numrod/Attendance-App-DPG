cc151d287b07d3fbb9a9bb7e25ce2b539c86884e
Profiliu <muaprof2@gmail.com>
2021-11-05 21:09:37 +0200

    Added class for opening forms within main panel

---
+++ Attendance App/Attendance App/AppClasses/ChildForms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Attendance_App.AdminForms;
+using Attendance_App.LecturerForms;
+
+namespace Attendance_App.AppClasses
+{
+    class ChildForms
+    {
+        private Form activeForm = null;
+        public void openChildForm(Form childForm, Panel childPanel)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            childPanel.Controls.Add(childForm);
+            childPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+    }
+}
