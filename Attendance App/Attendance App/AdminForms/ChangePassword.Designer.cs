db5b8ad28abfe8de750e32b8125a045b94ce332e
Profiliu <muaprof2@gmail.com>
2021-11-05 18:07:49 +0200

    Added Admin Update Account and Change Password Forms (no logic)

---
+++ Attendance App/Attendance App/AdminForms/ChangePassword.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Attendance_App.AdminForms
+{
+    partial class ChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChangePasswordText = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPreviousPassword = new System.Windows.Forms.Label();
+            this.lblNewPassword = new System.Windows.Forms.Label();
+            this.lblPasswordConfirm = new System.Windows.Forms.Label();
+            this.txtBoxPreviousPassword = new System.Windows.Forms.TextBox();
+            this.txtBoxNewPassword = new System.Windows.Forms.TextBox();
+            this.txtBoxPasswordConfirm = new System.Windows.Forms.TextBox();
+            this.btnChangePassword = new System.Windows.Forms.Button();
+            this.btnCloseForm = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ChangePasswordText
+            // 
+            this.ChangePasswordText.AutoSize = true;
+            this.ChangePasswordText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangePasswordText.Location = new System.Drawing.Point(250, 76);
+            this.ChangePasswordText.Name = "ChangePasswordText";
+            this.ChangePasswordText.Size = new System.Drawing.Size(236, 31);
+            this.ChangePasswordText.TabIndex = 0;
+            this.ChangePasswordText.Text = "Change Password";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(168)))), ((int)(((byte)(172)))));
+            this.panel1.Controls.Add(this.txtBoxPasswordConfirm);
+            this.panel1.Controls.Add(this.txtBoxNewPassword);
+            this.panel1.Controls.Add(this.txtBoxPreviousPassword);
+            this.panel1.Controls.Add(this.lblPasswordConfirm);
+            this.panel1.Controls.Add(this.lblNewPassword);
+            this.panel1.Controls.Add(this.lblPreviousPassword);
+            this.panel1.Location = new System.Drawing.Point(124, 145);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 272);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblPreviousPassword
+            // 
+            this.lblPreviousPassword.AutoSize = true;
+            this.lblPreviousPassword.Location = new System.Drawing.Point(61, 72);
+            this.lblPreviousPassword.Name = "lblPreviousPassword";
+            this.lblPreviousPassword.Size = new System.Drawing.Size(162, 17);
+            this.lblPreviousPassword.TabIndex = 0;
+            this.lblPreviousPassword.Text = "Enter Current Password:";
+            // 
+            // lblNewPassword
+            // 
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.Location = new System.Drawing.Point(61, 129);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(142, 17);
+            this.lblNewPassword.TabIndex = 1;
+            this.lblNewPassword.Text = "Enter New Password:";
+            // 
+            // lblPasswordConfirm
+            // 
+            this.lblPasswordConfirm.AutoSize = true;
+            this.lblPasswordConfirm.Location = new System.Drawing.Point(61, 186);
+            this.lblPasswordConfirm.Name = "lblPasswordConfirm";
+            this.lblPasswordConfirm.Size = new System.Drawing.Size(156, 17);
+            this.lblPasswordConfirm.TabIndex = 2;
+            this.lblPasswordConfirm.Text = "Confirm New Password:";
+            // 
+            // txtBoxPreviousPassword
+            // 
+            this.txtBoxPreviousPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPreviousPassword.Location = new System.Drawing.Point(226, 68);
+            this.txtBoxPreviousPassword.Name = "txtBoxPreviousPassword";
+            this.txtBoxPreviousPassword.Size = new System.Drawing.Size(226, 24);
+            this.txtBoxPreviousPassword.TabIndex = 3;
+            // 
+            // txtBoxNewPassword
+            // 
+            this.txtBoxNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNewPassword.Location = new System.Drawing.Point(226, 125);
+            this.txtBoxNewPassword.Name = "txtBoxNewPassword";
+            this.txtBoxNewPassword.Size = new System.Drawing.Size(226, 24);
+            this.txtBoxNewPassword.TabIndex = 3;
+            // 
+            // txtBoxPasswordConfirm
+            // 
+            this.txtBoxPasswordConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPasswordConfirm.Location = new System.Drawing.Point(226, 182);
+            this.txtBoxPasswordConfirm.Name = "txtBoxPasswordConfirm";
+            this.txtBoxPasswordConfirm.Size = new System.Drawing.Size(226, 24);
+            this.txtBoxPasswordConfirm.TabIndex = 3;
+            // 
+            // btnChangePassword
+            // 
+            this.btnChangePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(180)))), ((int)(((byte)(189)))));
+            this.btnChangePassword.FlatAppearance.BorderSize = 0;
+            this.btnChangePassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChangePassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangePassword.ForeColor = System.Drawing.Color.White;
+            this.btnChangePassword.Location = new System.Drawing.Point(472, 453);
+            this.btnChangePassword.Name = "btnChangePassword";
+            this.btnChangePassword.Size = new System.Drawing.Size(161, 39);
+            this.btnChangePassword.TabIndex = 4;
+            this.btnChangePassword.Text = "Change Password";
+            this.btnChangePassword.UseVisualStyleBackColor = false;
+            // 
+            // btnCloseForm
+            // 
+            this.btnCloseForm.BackColor = System.Drawing.Color.Crimson;
+            this.btnCloseForm.FlatAppearance.BorderSize = 0;
+            this.btnCloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseForm.ForeColor = System.Drawing.Color.White;
+            this.btnCloseForm.Location = new System.Drawing.Point(681, 21);
+            this.btnCloseForm.Name = "btnCloseForm";
+            this.btnCloseForm.Size = new System.Drawing.Size(32, 32);
+            this.btnCloseForm.TabIndex = 11;
+            this.btnCloseForm.Text = "✖";
+            this.btnCloseForm.UseVisualStyleBackColor = false;
+            // 
+            // ChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(744, 611);
+            this.Controls.Add(this.btnCloseForm);
+            this.Controls.Add(this.btnChangePassword);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ChangePasswordText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ChangePassword";
+            this.Text = "ChangePassword";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ChangePasswordText;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtBoxPasswordConfirm;
+        private System.Windows.Forms.TextBox txtBoxNewPassword;
+        private System.Windows.Forms.TextBox txtBoxPreviousPassword;
+        private System.Windows.Forms.Label lblPasswordConfirm;
+        private System.Windows.Forms.Label lblNewPassword;
+        private System.Windows.Forms.Label lblPreviousPassword;
+        private System.Windows.Forms.Button btnChangePassword;
+        private System.Windows.Forms.Button btnCloseForm;
+    }
+}
---
+++ Attendance App/Attendance App/AdminForms/UpdateAccount.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Attendance_App.AdminForms
+{
+    partial class UpdateAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateAccountText = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPhoneNo = new System.Windows.Forms.Label();
+            this.txtBoxNewNumber = new System.Windows.Forms.TextBox();
+            this.GotoPassChangeText = new System.Windows.Forms.Label();
+            this.btnGoToChangePassword = new System.Windows.Forms.Button();
+            this.btnUpdateNumber = new System.Windows.Forms.Button();
+            this.btnCloseForm = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UpdateAccountText
+            // 
+            this.UpdateAccountText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UpdateAccountText.AutoSize = true;
+            this.UpdateAccountText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateAccountText.Location = new System.Drawing.Point(270, 63);
+            this.UpdateAccountText.Name = "UpdateAccountText";
+            this.UpdateAccountText.Size = new System.Drawing.Size(208, 31);
+            this.UpdateAccountText.TabIndex = 0;
+            this.UpdateAccountText.Text = "Update Account";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(168)))), ((int)(((byte)(172)))));
+            this.panel1.Controls.Add(this.btnGoToChangePassword);
+            this.panel1.Controls.Add(this.GotoPassChangeText);
+            this.panel1.Controls.Add(this.txtBoxNewNumber);
+            this.panel1.Controls.Add(this.lblPhoneNo);
+            this.panel1.Location = new System.Drawing.Point(82, 119);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(593, 221);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblPhoneNo
+            // 
+            this.lblPhoneNo.AutoSize = true;
+            this.lblPhoneNo.Location = new System.Drawing.Point(110, 50);
+            this.lblPhoneNo.Name = "lblPhoneNo";
+            this.lblPhoneNo.Size = new System.Drawing.Size(107, 17);
+            this.lblPhoneNo.TabIndex = 0;
+            this.lblPhoneNo.Text = "Mobile Number:";
+            // 
+            // txtBoxNewNumber
+            // 
+            this.txtBoxNewNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNewNumber.Location = new System.Drawing.Point(238, 50);
+            this.txtBoxNewNumber.Name = "txtBoxNewNumber";
+            this.txtBoxNewNumber.Size = new System.Drawing.Size(259, 24);
+            this.txtBoxNewNumber.TabIndex = 1;
+            // 
+            // GotoPassChangeText
+            // 
+            this.GotoPassChangeText.AutoSize = true;
+            this.GotoPassChangeText.Location = new System.Drawing.Point(110, 119);
+            this.GotoPassChangeText.Name = "GotoPassChangeText";
+            this.GotoPassChangeText.Size = new System.Drawing.Size(248, 17);
+            this.GotoPassChangeText.TabIndex = 2;
+            this.GotoPassChangeText.Text = "To change your password press here:";
+            // 
+            // btnGoToChangePassword
+            // 
+            this.btnGoToChangePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(180)))), ((int)(((byte)(189)))));
+            this.btnGoToChangePassword.FlatAppearance.BorderSize = 0;
+            this.btnGoToChangePassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGoToChangePassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoToChangePassword.ForeColor = System.Drawing.Color.White;
+            this.btnGoToChangePassword.Location = new System.Drawing.Point(379, 134);
+            this.btnGoToChangePassword.Name = "btnGoToChangePassword";
+            this.btnGoToChangePassword.Size = new System.Drawing.Size(161, 39);
+            this.btnGoToChangePassword.TabIndex = 3;
+            this.btnGoToChangePassword.Text = "Change Password";
+            this.btnGoToChangePassword.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateNumber
+            // 
+            this.btnUpdateNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(106)))), ((int)(((byte)(108)))));
+            this.btnUpdateNumber.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateNumber.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateNumber.Location = new System.Drawing.Point(530, 358);
+            this.btnUpdateNumber.Name = "btnUpdateNumber";
+            this.btnUpdateNumber.Size = new System.Drawing.Size(145, 39);
+            this.btnUpdateNumber.TabIndex = 3;
+            this.btnUpdateNumber.Text = "Update Number";
+            this.btnUpdateNumber.UseVisualStyleBackColor = false;
+            // 
+            // btnCloseForm
+            // 
+            this.btnCloseForm.BackColor = System.Drawing.Color.Crimson;
+            this.btnCloseForm.FlatAppearance.BorderSize = 0;
+            this.btnCloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseForm.ForeColor = System.Drawing.Color.White;
+            this.btnCloseForm.Location = new System.Drawing.Point(681, 21);
+            this.btnCloseForm.Name = "btnCloseForm";
+            this.btnCloseForm.Size = new System.Drawing.Size(32, 32);
+            this.btnCloseForm.TabIndex = 10;
+            this.btnCloseForm.Text = "✖";
+            this.btnCloseForm.UseVisualStyleBackColor = false;
+            // 
+            // UpdateAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(744, 611);
+            this.Controls.Add(this.btnCloseForm);
+            this.Controls.Add(this.btnUpdateNumber);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.UpdateAccountText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "UpdateAccount";
+            this.Text = "UpdateAccount";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UpdateAccountText;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblPhoneNo;
+        private System.Windows.Forms.Button btnGoToChangePassword;
+        private System.Windows.Forms.Label GotoPassChangeText;
+        private System.Windows.Forms.TextBox txtBoxNewNumber;
+        private System.Windows.Forms.Button btnUpdateNumber;
+        private System.Windows.Forms.Button btnCloseForm;
+    }
+}
