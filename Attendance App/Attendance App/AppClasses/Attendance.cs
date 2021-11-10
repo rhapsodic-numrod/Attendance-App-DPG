9b76398e249656a9c1ef2f96cbf4b7e37c758983
Profiliu <muaprof2@gmail.com>
2021-11-10 15:46:57 +0200

    Added Attendance Class to project

---
+++ Attendance App/Attendance App/AppClasses/Attendance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendance_App.AppClasses
+{
+    public class Attendance
+    {
+        public static bool canTakeAttendance = false;
+    }
+
+    public class PasswordForm
+    {
+        public static bool canOpenForm = false;
+    }
+}
